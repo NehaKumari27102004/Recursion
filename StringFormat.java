7ecb9822cabfa440026145a7439333ba567e67aa
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-11-10 19:59:09 +0530

    Add files via upload

---
+++ StringFormat.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+public class StringFormat {
+    public static void main(String[] args) {
+        String name="Ram";
+        int marks=23;
+        System.out.println("My name is a " + name + " and my marks is " + marks);
+        System.out.printf("My name is %s ,and marks is %d :",name, marks);
+    }
+}
