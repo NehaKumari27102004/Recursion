09bd4b2199df13c1e6aa4619c5ce0f627e3fccc6
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-11-11 16:58:40 +0530

    Add files via upload

---
+++ StringTest.java
@@ -0,0 +1,11 @@
+package Recursion;
+
+public class StringTest {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("First");
+        sb.append(",How are you guy's");
+        sb.append(" I am a 45 years old ");
+        sb.append( 124467);
+        System.out.println(sb);
+    }
+}
